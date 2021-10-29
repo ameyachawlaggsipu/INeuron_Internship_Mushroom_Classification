748490ca6e1bcf2bae48008c4039871a8e06c5c9
Ameya Chawla <ameyachawla00@gmail.com>
2021-10-29 14:28:35 +0530

    WHole Mushroom Classification Application

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template,request
+import os
+from werkzeug.utils import secure_filename
+import numpy
+import joblib 
+from Logger.logger import Logs
+import pandas as pd
+app=Flask(__name__)
+log = Logs("test_logs.log")
+log.addLog("INFO", "Execution started Successfully !")
+model = joblib.load('Best_Model_Random_forest_Max_depth_72.pkl')
+log.addLog("INFO", "Best_Model_Random_forest_Max_depth_72.pkl loaded Successfully !")
+print(model)
+
+
+
+@app.route('/')
+def home():
+    log.addLog("INFO", "Rendering template <index.html> !")
+    return render_template('index.html')
+
+@app.route('/success', methods = ['POST']) 
+def predict():
+    if request.method == 'POST': 
+                message=request.files["file"]
+                message.save(message.filename)
+                log.addLog("INFO", "Information collected Successfully !")
+                o=output(message.filename)
+                print(message.filename)
+                print(o[0])
+                os.remove(message.filename)
+                r=[]
+                for j in range(len(o)):
+                    r.append('Predicted Class '+str(o[j])+' for Sample '+str(j))
+                print(r)
+                log.addLog("INFO", "Predicted Successfully !")
+                log.addLog("INFO", "Rendering template <index2.html> !")
+                return render_template('index2.html' , data=r)
+def output(file):
+    data=pd.read_csv(file)
+    data=numpy.array(data)
+    print( data.shape)
+    log.addLog("INFO", "Prediction started Successfully !")
+    prediction=model.predict(data)
+
+    return prediction 
+
+if __name__=="__main__":
+    app.run(host="localhost", port=8000, debug=True)
